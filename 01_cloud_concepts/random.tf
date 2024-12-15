c1efc5d197c0b59182bcef2d99a437959d0d0bcf
llenoir <llenoir@ippon.fr>
2024-12-15 13:44:00 +0100

    fix: took out unwanted solution

--- 01_cloud_concepts/random.tf
+++ 01_cloud_concepts/random.tf
@@ -1,12 +1,4 @@
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
 # You need to create a random integer below this comment
-resource "random_integer" "random" {
-  min = 10
-  max = 20
-}
 
 # You need to create a Terraform output of the result of your random integer below this comment
-output "random_integer" {
-  value = random_integer.random.result
-}
-
